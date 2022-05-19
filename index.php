f71bed21fc4792a4dfc7fd8e8cd32a0c08236f37
Brian <blianoldenzeel@gmail.com>
2022-05-19 10:27:07 +0200

    .

--- index.php
+++ index.php
@@ -10,19 +10,33 @@ $slogan ="I'm loving it";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/3ee23cac4e.js" crossorigin="anonymous"></script>
     <title>Webshop</title>
 </head>
 <body>
-    <span>
-        <ul class="header_nav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
+<span class="nav-container">
+        <header>
+                <ul class="nav_links2">
+                    <li><a href="">Shop</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <ul class="nav_links">
+                    <li><a href=""><i class="fa-solid fa-cart-shopping"></i></a></li>
+                    <li><a href=""><i class="fa-solid fa-bookmark"></i></a></li>
+                    <li><a href=""><i class="fa-solid fa-user"></i></a></li>
+                </ul>               
+        </header>
     </span>
 
     <span>
-    <div class="grid-project">
+        <div class="banner">
+            <img src="" alt="">
+        </div>
+    </span>
+
+    <span>
+        <div class="grid-project">
             <div class="project-item"><a href="schoenen.php"><img src="img/Schoenen.png" alt=""></a></div>
             <div class="project-item"><a href="broek.php"><img src="img/Broek.png" alt=""></a></div>
             <div class="project-item"><a href="shirt.php"><img src="img/shirt.png" alt=""></a></div>
