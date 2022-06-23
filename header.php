7951ebc7292fe61c221141fd8d1bfc16be70a1f8
Brian <blianoldenzeel@gmail.com>
2022-06-23 11:47:17 +0200

    .

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ $producten = array("Crossaint", "Baguette", "Brood");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://kit.fontawesome.com/3ee23cac4e.js" crossorigin="anonymous"></script>
-    <title>Webshop</title>
+    <title>Broodshop👍</title>
 </head>
 <body>
 <section class="nav-container">
